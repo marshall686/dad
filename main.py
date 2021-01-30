2dee7e7ef4005a69ecde77e31ba93f89b91246b9
marshall686 <74837890+marshall686@users.noreply.github.com>
2021-01-30 19:09:02 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,52 @@
+import socket
+import threading
+
+host = '127.0.0.1'
+port = 9090
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((host, port))
+
+server.listen()
+
+clients = []
+nicknames = []
+
+def broadcast(message):
+    for client in clients:
+        client.send(message)
+
+def handle(client):
+    while True:
+        try:
+            message = client.recv(1024)
+            print(f"{nicknames[clients.index(client)]} says {message}")
+            broadcast(message)
+        except:
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nickname = nicknames[index]
+            nicknames.remove(nickname)
+            break
+
+def receive():
+    while(True):
+        client, address = server.accept()
+        print(f"connected with {str(address)}")
+
+        client.send("NICK".encode('utf-8'))
+        nickname = client.recv(1024)
+
+        nicknames.append(nickname)
+        clients.append(client)
+
+        print(f"nickname of the client is {nickname}")
+        broadcast(f"{nickname} is connected\n".encode('utf-8'))
+        client.send("you are now connected to the server".encode('utf-8'))
+
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
+
+print("server running...")
+receive()
